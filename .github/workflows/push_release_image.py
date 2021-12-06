1476bf6187541552704b4091b4f9394dcff1f7fe
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2021-12-06 12:13:08 +0100

    Build docker image for release (#17)

---
+++ .github/workflows/push_release_image.py
@@ -0,0 +1,18 @@
+"""Run from the root of the repository"""
+import os
+from pathlib import Path
+
+from python_on_whales import docker
+
+
+def get_version() -> str:
+    return Path("./VERSION.txt").read_text().strip()
+
+
+def main():
+    version = get_version()
+    docker.login(
+        username=os.environ["DOCKERHUB_USERNAME"],
+        password=os.environ["DOCKERHUB_PASSWORD"],
+    )
+    docker.buildx.bake(["deploy-image"], push=True, variables={"TAG": version})
